c4b20dddd93ca3c771ab07cd38f774f7c11f2c15
noahshpak <noah@iterationlab.org>
2025-02-16 14:06:49 -0800

    patch

--- src/conversions/uuid.rs
+++ src/conversions/uuid.rs
@@ -31,11 +31,7 @@ static UUID_CLS: GILOnceCell<Py<PyType>> = GILOnceCell::new();
 
 #[inline(always)]
 fn get_uuid_cls(py: Python<'_>) -> PyResult<&Bound<'_, PyType>> {
-    UUID_CLS.get_or_try_init(py, || {
-        py.import("uuid")?
-            .getattr("UUID")?
-            .extract()
-    })
+    UUID_CLS.get_or_try_init_type_ref(py, "uuid", "UUID")
 }
 
 #[inline(always)]
